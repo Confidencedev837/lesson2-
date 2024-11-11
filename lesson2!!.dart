e8a873f91fb3b1ffe740d4a894c20faec8aac739
Confidencedev837 <confidenceorok30@gmail.com>
2024-11-11 15:18:46 +0100

    Add files via upload

---
+++ lesson2!!.dart
@@ -0,0 +1,31 @@
+void main() {
+                       //VARIABLES
+  //THEY ARE OBJECTS IN WHICH YOU CAN STORE DIFFERENT TYPES
+  //OF INF0RMATION WITHIN THEM
+ String name="Orok Confidence";
+  int age=17;
+  double pi=3.14159;
+  bool isadvanced=true;
+print(name);
+  print(age);
+  print(pi);
+  print(isadvanced);
+
+                  //BASIC MATHS OPERATORS
+  int y = 15;
+  int x = 4;
+  int sum = y + x;
+  print(sum);
+
+  print("${y + x}"); //addition
+  print("${y - x}"); //subtraction
+  print("${y * x}"); //multiplication
+  print("${y / x}"); //division
+  print("${y ~/ x}"); // intregral
+  print("${y % x}"); // modular operator
+  y++;
+  print(y);// increment
+  
+  x--;
+  print(x);
+}
